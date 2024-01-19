a036b4ff81b00efc77d4bd7584e7ef6c0d9dbe3e
Narwhal Studios <cursedpong@gmail.com>
2024-01-18 21:33:51 -0800

    updating

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
     import NavBar from '$lib/NavBar.svelte';
-    import flag from '$lib/flag.png';
+    import ofnale from '$lib/ofnale.png';
 </script>
 <title>Ofnale</title>
 
 <NavBar />
 
-<p>Pour apprendre plus à propos d'Ofnale, visite un des autres catégoires sur cet site-web.</p>
+<p>Pour apprendre plus à propos d'Ofnale, visite un des autres catégories sur ce site web.</p>
+
+<img src={ofnale} alt="nature pic" />
+
+<style>
+    img {
+        width: 40%;
+    }
+</style>
--- src/routes/hobbies/+page.svelte
+++ src/routes/hobbies/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import NavBar from '$lib/NavBar.svelte';
+    import nature from '$lib/nature.png';
 </script>
 <title>Loisirs</title>
 
@@ -8,3 +9,11 @@
 <h1>Losirs</h1>
 
 <p>	Dans Ofnale, il y a beaucoup de loisirs communs. Mais, il y a un loisir qui est le plus commun. Ce loisir est la nature. Ofnale à une forêt et une montagne qui sont très beaux, donc les promenades sont populaires. Cette nature est très proche aux villes.</p>
+
+<img src={nature} alt="nature" />
+
+<style>
+    img {
+        width: 30%;
+    }
+</style>
