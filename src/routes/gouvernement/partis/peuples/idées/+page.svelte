6b6a45db73bfc3120888bb1561433f131a2692d2
Narwhal Studios <cursedpong@gmail.com>
2024-01-26 13:13:30 -0800

    Updating

--- src/routes/gouvernement/partis/peuples/idées/+page.svelte
+++ src/routes/gouvernement/partis/peuples/idées/+page.svelte
@@ -9,6 +9,6 @@
 
 <h1>Nos Idées Centrales</h1>
 
-<p>Notre idée centrale, est qu'Ofnale devrait avoir plus de villes et peuples. Nous pensons qu'Ofnale devrait avoir plus de villes et peuples, car l'infrastructure est moins chère quand c'est dans un endroit centrale. Une autre de nos idées centrales est que nous voulons créer beaucoup de travaux pour les personnes.</p>
+<p>Notre idée centrale, est qu'Ofnale devrait avoir plus de villes et peuples. Nous n'aimons pas les taxes. Nous pensons qu'Ofnale devrait avoir plus de villes et peuples, car l'infrastructure est moins chère quand c'est dans un endroit centrale. Si l'infrastructure est moins chère, moins de taxes. Une autre de nos idées centrales est que nous voulons créer beaucoup de travaux pour les personnes.</p>
 
 <br><br><br><br><br><br><br><br>
